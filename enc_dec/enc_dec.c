138fe7fb8b2a850b2190762dfa73f5d34df82e70
abderrahim <jfjabde@gmail.com>
2025-11-29 00:23:50 +0100

    last_test

--- enc_dec/enc_dec.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void encode(const char *input, char *output) {
-    // Simple encoding: shift each character by 1
-    while (*input) {
-        *output++ = *input++ + 1;
-    }
-    *output = '\0';
-}
