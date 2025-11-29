5629b3dfe3b4ad47ce30c412f7ebc39ad515806b
Karim <mmnwr115@gmail.com>
2025-11-29 00:21:48 +0000

    Implement encoding function to shift characters by 1

--- enc_dec/enc_dec.c
+++ enc_dec/enc_dec.c
@@ -1,1 +1,9 @@
-ujyhyeygbsgffeyrfgj;eghqrj;hfgwrbsregvergzrfvhbnzrfgvrzugvrf
+#include <stdio.h>
+
+void encode(const char *input, char *output) {
+    // Simple encoding: shift each character by 1
+    while (*input) {
+        *output++ = *input++ + 1;
+    }
+    *output = '\0';
+}
