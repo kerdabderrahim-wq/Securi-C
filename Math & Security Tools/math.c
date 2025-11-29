661d0d7b3f241d38efcbc9ec3bfe1b1e953dfeb7
Karim <mmnwr115@gmail.com>
2025-11-29 00:54:25 +0000

    Fix is_prime function implementation for correct prime number detection

--- Math & Security Tools/math.c
+++ Math & Security Tools/math.c
@@ -11,3 +11,19 @@ int is_even(int a){
     
 }
 
+int is_prime(int a){
+    if (a<=1)
+    {
+        return 0;
+    }
+    for (int i = 2; i*i <= a; i++)
+    {
+        if (a%i==0)
+        {
+            return 0;
+        }
+        
+    }
+    return 1;
+    
+}
