45fbb2c3434b311968cd5c78297c2596e91b3a7c
abderrahim <jfjabde@gmail.com>
2025-12-01 21:31:55 +0100

    Enhance main menu options and improve user interaction in the Securi-C toolkit

--- main2.c
+++ main2.c
@@ -1,6 +1,8 @@
+
 #include <stdio.h>
 
 int main() {
+    int choice;
     printf("\n");
     printf("   _____                     _        _____ \n");
     printf("  / ____|                   (_)      / ____|\n");
@@ -13,24 +15,52 @@ int main() {
 
 
 
-
     printf("Welcome to Securi-C! Your command-line security toolkit.\n");
     printf("Choose an option to get started:\n");
     printf("1. Math & Security Tools\n");   
     printf("2. Encryption & Decryption\n");
     printf("Enter your choice (1 or 2): ");
-    int choice;
+    
     scanf("%d", &choice);
     if (choice == 1) {
         printf("Launching Math & Security Tools...\n");
-        // Here you would call the Math & Security Tools module
+        printf("Choose one of these modules:\n");
+        printf("1. isEven\n");
+        printf("2. isPrime\n");
+        printf("3. GCD\n");
+        printf("4. lcm\n");
+        printf("5. power\n");
+        printf("6. modExp\n");
+        printf("7. factorial\n");
+        printf("8. sumDigits\n");
+        printf("9. reverseNumber\n"); 
+        printf("10. isPalindromeNumber\n");
+        printf("11. sumDivisors\n");
+        printf("12. isPerfectNumber\n");
+        printf("13. isArmstrong\n");
+        printf("14. randomNumber\n");
+        printf("15. sumArray\n");
+        printf("16. averageArray\n");
+        printf("17. maxArray\n");
+        printf("18. minArray\n");
+        printf("19. sortAscending\n");
+
     } else if (choice == 2) {
         printf("Launching Encryption & Decryption...\n");
-        // Here you would call the Encryption & Decryption module
+        printf("Choose one of these modules:\n");
+        printf("1. toUppercase\n");   
+        printf("2. toLowercase\n");
+        printf("3. reverseMessage\n");
+        printf("4. removeSpaces\n");
+        printf("5. encryptCesar\n");
+        printf("6. decryptCesar\n");
+        printf("7. encryptXOR\n");
+        printf("8. decryptXOR\n");
+        printf("9. encryptSubstitution\n");
+        printf("10. decryptSubstitution\n");
     } else {
         printf("Invalid choice. Please restart the program and select 1 or 2.\n");
     }
-    
     return 0;
 
 
