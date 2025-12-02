8e48d6be25beeb302b396136b85e07ddd110c863
Karim <mmnwr115@gmail.com>
2025-12-02 20:49:49 +0000

    Add header file for encryption and decryption functions

---
+++ Encryption & Decryption/enc_dec.h
@@ -0,0 +1,21 @@
+
+void inputMessage(struct Message *m);
+void displayMessage(struct Message m);
+int isUppercase(char c);   
+int isLowercase(char c);
+int isAlphabetic(char c);
+void toUppercase(struct Message *m);
+void toLowercase(struct Message *m);
+void reverseMessage(struct Message *m);
+void removeSpaces(struct Message *m);
+void encryptCesar(struct Message *m, int key);
+void decryptCesar(struct Message *m, int key);
+void encryptXOR(struct Message *m, int key);
+void decryptXOR(struct Message *m, int key);
+void encryptSubstitution(struct Message *m, char key[26]);
+void decryptSubstitution(struct Message *m, char key[26]);
+int isValidKey(char key[26]);
+int compareMessages(struct Message m1, struct Message m2);
+int countCharacter(struct Message m, char c);
+void frequencyAnalysis(struct Message m);
+float coincidenceIndex(struct Message m);
